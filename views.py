3a0960d9d151f843a1fab4139dc797368ca41a73
Jensen David <feudavid92@gmail.com>
2020-04-14 20:38:17 -0700

    Updated templating

--- views.py
+++ views.py
@@ -3,25 +3,16 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    home_html = open('content/index.html').read()
-    context = {
-    'content': home_html
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'index.html', context)
 
 def contact(request):
-    contact_html = open('content/contact.html').read()
-    context = {
-    'content': contact_html
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'contact.html', context)
 
 def projects(request):
-    projects_html = open('content/projects.html').read()
-    context = {
-    'content': projects_html
-    }
-    return render(request, 'base.html', context)
+    context = {}
+    return render(request, 'projects.html', context)
 
 
 def github_api(request):
